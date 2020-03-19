ab5176090574f4e48d7297c00cbbb86f3c41ed53
Ondřej Garncarz <ondrej@garncarz.cz>
2020-03-20 00:59:38 +0100

    Init.

---
+++ README.md
@@ -0,0 +1,7 @@
+# TensorFlow learning lab
+
+Following https://lab.github.com/everydeveloper/advance-tensorflow.
+
+`pip install -r requirements.txt`
+
+`./app.py`
---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
